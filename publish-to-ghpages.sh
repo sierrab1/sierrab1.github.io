d8a5afc16d45f044aa394de6bef9cf7b53442cb7
Sierra Barkdoll <sierrabarkdoll@yahoo.com>
2020-02-02 18:19:03 -0500

    add script from Hugo guide for publishing updates

---
+++ publish-to-ghpages.sh
@@ -0,0 +1,30 @@
+
+#!/bin/sh
+
+if [ "`git status -s`" ]
+then
+	echo "The working directory is dirty. Please commit any pending changes."
+	exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out publishing branch into public"
+git worktree add -B master public origin/master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+../hugo
+
+echo "Updating publish branch (for you it's master)"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
+
+echo "Pushing to GitHub"
+git push --all
+
